27df09656ba4a0c77a591800d064f834b30a0ffe
ovrdc <gis.ovrdc@gmail.com>
2015-01-15 20:53:30 -0500

    Update leaflet.social.js

--- src/leaflet.social.js
+++ src/leaflet.social.js
@@ -1,7 +1,7 @@
 L.Control.Social = L.Control.extend({
   includes: L.Mixin.Events,
   options: {
-    position: 'topright',
+    position: 'bottomleft',
     default_text: "What a nice map",
     links: [
       ['facebook', "Facebook", "https://www.facebook.com/sharer.php?u=_url_&t=_text_"],
